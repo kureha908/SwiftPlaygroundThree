239b0e6c0331cf6cbe0bcb26b4b14435fe62dea2
Kureha <kureha@hawaii.edu>
2019-02-14 10:30:43 -1000

    Complete class assignment week 6

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -2,3 +2,22 @@ import UIKit
 
 var str = "Hello, playground"
 
+
+//Part 2
+//declaration
+var friutNames:[String] = ["Peach","Strawberry","Watermelon"]
+//assign firutNames  qith 3 of  your favorite friuts
+
+//Part 4
+print(friutNames)
+print("I like to eat \(friutNames)")
+//Part 6
+for favorits in friutNames{
+    print("I like to eat " + favorits)
+}
+//Part 7
+print("I like to eat \(friutNames[1])")
+print("I like to eat \(friutNames[0])")
+
+
+//Assignment 4
