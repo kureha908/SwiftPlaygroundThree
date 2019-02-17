b2a3a9cbf25f58e53f15eb02361c4f57abb51b48
Kureha <kureha@hawaii.edu>
2019-02-17 01:47:54 -1000

    Complete Assignment 4

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -20,16 +20,21 @@ print("I like to eat \(friutNames[1])")
 print("I like to eat \(friutNames[0])")
 
 
-//Part 8, Start of Assignment #4
+//Assignment #4
 
 var number :[integer_t] = [2,3,8,9,10]
 print(number)
+print("My favorit number are \(number)")
 for myFavoritNumber in number{
     print("My favorite number is", myFavoritNumber)
 }
+print("My favorit number is \(number[1])")
+print("My favorit number is \(number[0])")
 
 //Part 9
 let removed = number.remove(at: 2)
+print(number)
+
 for myFavoritNumber in number{
-    print(myFavoritNumber)
+    print("My favorite number is",myFavoritNumber)
 }
